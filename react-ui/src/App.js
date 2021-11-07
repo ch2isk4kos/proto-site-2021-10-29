6056d85df100d10e1ba66d72d5f63f02412b42c5
ch2isk4kos <ch2isk4kos@gmail.com>
2021-11-06 20:36:00 -0400

    test various layouts

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,118 +1,13 @@
-import React, { useState } from "react";
-import { Layout, Menu, Breadcrumb } from "antd";
-import {
-  DesktopOutlined,
-  PieChartOutlined,
-  FileOutlined,
-  TeamOutlined,
-  UserOutlined,
-  SettingOutlined,
-  MailOutlined,
-  AppstoreOutlined,
-} from "@ant-design/icons";
+import React from "react";
+import Layout from "./components/layout/Layout";
 import "./App.css";
 
-const { Header, Content, Footer, Sider } = Layout;
-const { SubMenu } = Menu;
-
-function App() {
-  const [currentNavLink, setIsCurrentNavLink] = useState("");
-  const [isCollapsed, setIsCollapsed] = useState(false);
-
-  const handleOnCollapse = (bool) => {
-    setIsCollapsed(bool);
-  };
-
-  const handleOnNavLink = (e) => {
-    setIsCurrentNavLink({ current: e.key });
-  };
-
+const App = () => {
   return (
     <div className="App">
-      <Menu
-        onClick={handleOnNavLink}
-        selectedKeys={[currentNavLink]}
-        mode="horizontal"
-      >
-        <Menu.Item key="mail" icon={<MailOutlined />}>
-          Navigation One
-        </Menu.Item>
-        <Menu.Item key="app" disabled icon={<AppstoreOutlined />}>
-          Navigation Two
-        </Menu.Item>
-        <SubMenu
-          key="SubMenu"
-          icon={<SettingOutlined />}
-          title="Navigation Three - Submenu"
-        >
-          <Menu.ItemGroup title="Item 1">
-            <Menu.Item key="setting:1">Option 1</Menu.Item>
-            <Menu.Item key="setting:2">Option 2</Menu.Item>
-          </Menu.ItemGroup>
-          <Menu.ItemGroup title="Item 2">
-            <Menu.Item key="setting:3">Option 3</Menu.Item>
-            <Menu.Item key="setting:4">Option 4</Menu.Item>
-          </Menu.ItemGroup>
-        </SubMenu>
-        <Menu.Item key="alipay">
-          <a
-            href="https://ant.design"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Navigation Four - Link
-          </a>
-        </Menu.Item>
-      </Menu>
-      <Layout style={{ minHeight: "100vh" }}>
-        <Sider
-          collapsible
-          collapsed={isCollapsed}
-          onCollapse={handleOnCollapse}
-        >
-          <div className="logo" />
-          <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
-            <Menu.Item key="1" icon={<PieChartOutlined />}>
-              Option 1
-            </Menu.Item>
-            <Menu.Item key="2" icon={<DesktopOutlined />}>
-              Option 2
-            </Menu.Item>
-            <SubMenu key="sub1" icon={<UserOutlined />} title="User">
-              <Menu.Item key="3">Tom</Menu.Item>
-              <Menu.Item key="4">Bill</Menu.Item>
-              <Menu.Item key="5">Alex</Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
-              <Menu.Item key="6">Team 1</Menu.Item>
-              <Menu.Item key="8">Team 2</Menu.Item>
-            </SubMenu>
-            <Menu.Item key="9" icon={<FileOutlined />}>
-              Files
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout className="site-layout">
-          <Header className="site-layout-background" style={{ padding: 0 }} />
-          <Content style={{ margin: "0 16px" }}>
-            <Breadcrumb style={{ margin: "16px 0" }}>
-              <Breadcrumb.Item>User</Breadcrumb.Item>
-              <Breadcrumb.Item>Bill</Breadcrumb.Item>
-            </Breadcrumb>
-            <div
-              className="site-layout-background"
-              style={{ padding: 24, minHeight: 360 }}
-            >
-              Bill is a cat.
-            </div>
-          </Content>
-          <Footer style={{ textAlign: "center" }}>
-            MSJ ©2021 Created by Chris Kakos
-          </Footer>
-        </Layout>
-      </Layout>
+      <Layout />
     </div>
   );
-}
+};
 
 export default App;
