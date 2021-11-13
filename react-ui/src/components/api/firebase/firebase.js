3b44e065fd7588dcd090d00404346543b3978854
ch2isk4kos <ch2isk4kos@gmail.com>
2021-11-13 17:47:40 -0500

    add directory for api communications

---
+++ react-ui/src/components/api/firebase/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/firebase";
+
+const config = {
+  apiKey: "AIzaSyA-rue-O4kbJIFNEmI91Dr07cNMcULe5aY",
+  authDomain: "proto-site-2021-10-29.firebaseapp.com",
+  projectId: "proto-site-2021-10-29",
+  storageBucket: "proto-site-2021-10-29.appspot.com",
+  messagingSenderId: "699197766001",
+  appId: "1:699197766001:web:62cbbcb2fb5a7c18731ed0",
+};
+
+// initialize
+firebase.initializeApp(config);
