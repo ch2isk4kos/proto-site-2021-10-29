cad4b0320f2eba190b420b7913bc5dbdb0ad63b1
ch2isk4kos <ch2isk4kos@gmail.com>
2021-11-12 22:21:51 -0500

    wrap application with react router parent component

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "./index.css";
 
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
+  // </React.StrictMode>,
   document.getElementById("root")
 );
 
