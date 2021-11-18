408887a0bc9befba013eedfe05d02f84def74d4f
ch2isk4kos <ch2isk4kos@gmail.com>
2021-11-18 18:13:16 -0500

    rename firebase config file

---
+++ react-ui/src/components/api/firebase/config.js
@@ -0,0 +1,39 @@
+// Firebase v9.4 imports
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  sendSignInLinkToEmail,
+  signInWithEmailLink,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyA-rue-O4kbJIFNEmI91Dr07cNMcULe5aY",
+  authDomain: "proto-site-2021-10-29.firebaseapp.com",
+  projectId: "proto-site-2021-10-29",
+  storageBucket: "proto-site-2021-10-29.appspot.com",
+  messagingSenderId: "699197766001",
+  appId: "1:699197766001:web:62cbbcb2fb5a7c18731ed0",
+};
+
+// initialize
+const app = initializeApp(config);
+const auth = getAuth(app);
+console.log("api/firebase: auth -> ", auth);
+
+export const provider = new GoogleAuthProvider();
+console.log("api/firebase: provider -> ", provider);
+
+// signup
+export const sendVerificationLink = async (email, options) => {
+  await sendSignInLinkToEmail(auth, email, options);
+};
+
+// signin
+export const signInWithEmail = async (email, location) => {
+  await signInWithEmailLink(email, location);
+};
+
+// create/update user
+
+// current user
