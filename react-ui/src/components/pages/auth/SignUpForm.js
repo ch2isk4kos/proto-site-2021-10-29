7af97a169605c74949b28d24f1523dc0d965fa93
ch2isk4kos <ch2isk4kos@gmail.com>
2021-11-20 17:23:49 -0500

    import react-redux hooks

--- react-ui/src/components/pages/auth/SignUpForm.js
+++ react-ui/src/components/pages/auth/SignUpForm.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import {
   getUserIdToken,
