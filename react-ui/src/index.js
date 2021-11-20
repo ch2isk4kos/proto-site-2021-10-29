c90fc1d420e9af66dd30ca0b9f762e81f5b7032d
ch2isk4kos <ch2isk4kos@gmail.com>
2021-11-20 17:59:30 -0500

    pass redux store as props to react-redux wrapper

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -14,7 +14,7 @@ const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   // <React.StrictMode>
-  <Provider>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
