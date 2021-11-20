0fd469903d8b2bfe931402d713530f7528a901d0
ch2isk4kos <ch2isk4kos@gmail.com>
2021-11-20 18:32:04 -0500

    import useDispatch hook from react-redux and firebase helper method to listen for user state

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import AppLayout from "./components/layout/AppLayout";
+import { onUserStateChange } from "./api/firebase/helpers";
 import "./App.css";
 
 const App = () => {
