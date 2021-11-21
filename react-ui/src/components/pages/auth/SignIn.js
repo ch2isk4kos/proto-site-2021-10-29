0fc32cd032b4c4b005c0474156e89beaaac9470d
ch2isk4kos <ch2isk4kos@gmail.com>
2021-11-20 23:00:38 -0500

    add structural styling for sign in form

--- react-ui/src/components/pages/auth/SignIn.js
+++ react-ui/src/components/pages/auth/SignIn.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { Form, Input, Button, Checkbox } from "antd";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import SignUp from "./SignUp";
 
 const { Item } = Form;
 
 const SignIn = () => {
-  const [form] = Form.useForm();
+  // const [form] = Form.useForm();
   const [, forceUpdate] = useState({}); // To disable submit button at the beginning.
 
   useEffect(() => {
@@ -21,132 +23,73 @@ const SignIn = () => {
   };
 
   return (
-    // <div className="signin">
-    //   <Form
-    //     className="signin-form"
-    //     form={form}
-    //     name="basic"
-    //     layout="inline"
-    //     onFinish={handleOnSubmit}
-    //     onFinishFailed={handleOnSubmitError}
-    //     autoComplete="off"
-    //   >
-    //     <Item
-    //       className="signin-form-item"
-    //       name="username"
-    //       rules={[
-    //         {
-    //           required: true,
-    //           message: "Please input your username!",
-    //         },
-    //       ]}
-    //     >
-    //       <Input
-    //         prefix={<UserOutlined className="site-form-item-icon" />}
-    //         placeholder="Username"
-    //       />
-    //     </Item>
-    //     <Item
-    //       className="signin-form-item"
-    //       name="password"
-    //       rules={[
-    //         {
-    //           required: true,
-    //           message: "Please input your password!",
-    //         },
-    //       ]}
-    //     >
-    //       <Input
-    //         prefix={<LockOutlined className="site-form-item-icon" />}
-    //         type="password"
-    //         placeholder="Password"
-    //       />
-    //     </Item>
-    //     <Item className="signin-form-item" shouldUpdate>
-    //       {() => (
-    //         <Button
-    //           type="primary"
-    //           htmlType="submit"
-    //           disabled={
-    //             !form.isFieldsTouched(true) ||
-    //             !!form.getFieldsError().filter(({ errors }) => errors.length)
-    //               .length
-    //           }
-    //         >
-    //           Log in
-    //         </Button>
-    //       )}
-    //     </Item>
-    //   </Form>
-    // </div>
     <div className="signin">
       <h1>Sign In.</h1>
       <Form
         className="signin-form"
-        name="basic"
-        labelCol={{
-          span: 8,
-        }}
-        wrapperCol={{
-          span: 16,
-        }}
+        name="normal_login"
         initialValues={{
           remember: true,
         }}
         onFinish={handleOnSubmit}
         onFinishFailed={handleOnSubmitError}
-        autoComplete="off"
       >
         <Item
           className="signin-form-item"
-          label="Username"
-          name="username"
+          name="email"
           rules={[
             {
               required: true,
-              message: "Please input your username!",
+              message: "Please input your Username!",
             },
           ]}
         >
-          <Input />
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="email@address.com"
+            autoFocus
+          />
         </Item>
-
         <Item
           className="signin-form-item"
-          label="Password"
           name="password"
           rules={[
             {
               required: true,
-              message: "Please input your password!",
+              message: "Please input your Password!",
             },
           ]}
         >
-          <Input.Password />
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Item>
+        <Item className="signin-form-item">
+          <Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox className="signin-form-remember">Remember me</Checkbox>
+          </Item>
+
+          <a className="signin-form-forgot" href="">
+            Forgot password
+          </a>
         </Item>
 
-        <Item
-          className="signin-form-item"
-          name="remember"
-          valuePropName="checked"
-          wrapperCol={{
-            offset: 8,
-            span: 16,
-          }}
-        >
-          <Checkbox>Remember me</Checkbox>
-        </Item>
-
-        <Item
-          className="signin-form-item"
-          wrapperCol={{
-            offset: 8,
-            span: 16,
-          }}
-        >
-          <Button type="primary" htmlType="submit">
-            Submit
+        <Item className="signin-form-item">
+          <Button
+            className="signin-form-button"
+            type="primary"
+            htmlType="submit"
+          >
+            Log in
           </Button>
+          <span className="signin-form-register">
+            Or{" "}
+            <Link to="/signup" element={<SignUp />}>
+              register now
+            </Link>
+          </span>
         </Item>
       </Form>
     </div>
