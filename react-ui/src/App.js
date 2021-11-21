1c85c6eae419f69abe33e5de7361f52df540f993
ch2isk4kos <ch2isk4kos@gmail.com>
2021-11-20 22:13:21 -0500

    add useEffect to listen for user state whenever the application reloads

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,10 +1,30 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import AppLayout from "./components/layout/AppLayout";
-import { onUserStateChange } from "./api/firebase/helpers";
+import { auth } from "./api/firebase/config";
+import { onAuthStateChanged } from "@firebase/auth";
 import "./App.css";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // listen for user state change
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      console.log("App -> unsubscribe -> user:", user);
+      if (!!user) {
+        dispatch({
+          type: "CURRENT_USER",
+          payload: {
+            email: user.email,
+            token: user.accessToken,
+          },
+        });
+      }
+    });
+    return () => unsubscribe();
+  }, [dispatch]);
+
   return (
     <div className="App">
       <AppLayout />
