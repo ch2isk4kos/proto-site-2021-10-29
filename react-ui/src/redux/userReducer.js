00d04a88067021f7816651b0fed3a7167f2beefd
ch2isk4kos <ch2isk4kos@gmail.com>
2021-11-22 18:33:44 -0500

    add redux SIGN_IN action type to user reducer

--- react-ui/src/redux/userReducer.js
+++ react-ui/src/redux/userReducer.js
@@ -2,6 +2,8 @@ export const userReducer = (state = null, action) => {
   switch (action.type) {
     case "CURRENT_USER":
       return action.payload;
+    case "SIGN_IN":
+      return action.payload;
     case "SIGN_OUT":
       return action.payload;
     default:
