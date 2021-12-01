6330586d9b4124de07c673dc2368b66e703b5fb5
ch2isk4kos <ch2isk4kos@gmail.com>
2021-12-01 18:51:11 -0500

    create component for feature test

---
+++ react-ui/src/components/layout/Ticker.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Ticker = () => {
+  return (
+    <div
+      className="ticker"
+      style={{ backgroundColor: "white", border: "3px solid red" }}
+    >
+      <h1>Ticker</h1>
+    </div>
+  );
+};
+
+export default Ticker;
