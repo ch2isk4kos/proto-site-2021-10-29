1c5a1a7082b7a7b9e7d172722093ce74749d7a22
ch2isk4kos <ch2isk4kos@gmail.com>
2021-12-01 18:52:54 -0500

    import component for feature test

--- react-ui/src/components/layout/AppLayout.js
+++ react-ui/src/components/layout/AppLayout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import NavBar from "./NavBar";
+import Ticker from "./Ticker";
 import AppContainer from "./AppContainer";
 import Footer from "./Footer";
 import { Layout } from "antd";
@@ -8,6 +9,7 @@ const AppLayout = () => {
   return (
     <Layout className="app-layout" style={{ border: "3px solid blue" }}>
       <NavBar />
+      <Ticker />
       <AppContainer />
       <Footer />
     </Layout>
