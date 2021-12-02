c4daf7e61e9ac790601e97902500c36682eaca4d
ch2isk4kos <ch2isk4kos@gmail.com>
2021-12-01 23:17:03 -0500

    apply template for ticker component

--- react-ui/src/components/layout/Ticker.js
+++ react-ui/src/components/layout/Ticker.js
@@ -2,10 +2,7 @@ import React from "react";
 
 const Ticker = () => {
   return (
-    <div
-      className="ticker"
-      style={{ backgroundColor: "white", border: "3px solid red" }}
-    >
+    <div className="ticker">
       <h1>Ticker</h1>
     </div>
   );
